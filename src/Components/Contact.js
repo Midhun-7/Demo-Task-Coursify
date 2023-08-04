96da24969fba02c24fcd0c484586e0e722b05b63
Midhun Muraleedharan <73743937+Midhun-7@users.noreply.github.com>
2023-08-04 22:23:11 +0530

    Update Demo Task

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,6 +4,7 @@ const Contact = () => {
   return (
     <div id="contact" className="contact">
       <div className="contact-page-wrapper">
+      <p className="primary-subheading">Contact</p>
         <h1 className="primary-heading">Have Question In Mind?</h1>
         <h1 className="primary-heading">Let Us Help You</h1>
         <div className="contact-form-container">
